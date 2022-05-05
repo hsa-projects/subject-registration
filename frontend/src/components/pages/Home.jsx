70b48ff3cc2a65c7c9fb01ff66a0b575da7a5fc8
hanjul <38674358+hanjul@users.noreply.github.com>
2022-05-05 23:22:40 +0200

    Fix useEffect hooks

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../layout/Navbar";
 import BurgerMenu from "../layout/BurgerMenu";
-import {URLS} from "../../App";
-import {RegistrationControllerApi, SubjectControllerApi} from "@/api";
-import {useContext, useEffect, useState} from "react";
+import { URLS } from "../../App";
+import { RegistrationControllerApi, SubjectControllerApi } from "@/api";
+import { useContext, useEffect, useState } from "react";
 import userContext from "../../context/userContext";
 import SubjectSelectionContext from "../../context/subjectSelectionContext";
-import {getRequestHeaders} from "../../util/util";
+import { getRequestHeaders } from "../../util/util";
 
-const IMPRINT_URL = 'https://www.hs-augsburg.de/Service/Impressum.html';
+const IMPRINT_URL = "https://www.hs-augsburg.de/Service/Impressum.html";
 
 /**
  * Main page of the application.
@@ -15,80 +15,100 @@ const IMPRINT_URL = 'https://www.hs-augsburg.de/Service/Impressum.html';
  * @constructor
  */
 function Home() {
-    const registrationApi = new RegistrationControllerApi();
-    const subjectApi = new SubjectControllerApi();
-    const {user, setUser} = useContext(userContext);
-    const [userInfo, setUserInfo] = useState(null);
-    const {subjectSelection, setSubjectSelection} = useContext(SubjectSelectionContext);
+  const registrationApi = new RegistrationControllerApi();
+  const subjectApi = new SubjectControllerApi();
+  const { user, setUser } = useContext(userContext);
+  const [userInfo, setUserInfo] = useState(null);
+  const { subjectSelection, setSubjectSelection } = useContext(
+    SubjectSelectionContext
+  );
 
-    // initially fetch the registration and subject selection of the logged in user
-    useEffect(() => {
-        if (user) {
-            return user.loadUserInfo().then((userInfo) => {
-                setUserInfo(userInfo);
-                if (!subjectSelection) {
-                    return registrationApi.getRegistration(user.subject, getRequestHeaders(user))
-                        .then((response) => {
-                            console.log(`registrations of user ${user.subject}: ${response.data}`);
-                            const registration = response.data;
-                            if (!registration) {
-                                return;
-                            }
-                            //setRegistration(registration);
-                            return subjectApi.getAllSubjects(getRequestHeaders(user))
-                                .then((response) => {
-                                    const userSubjects = [];
-                                    response.data.forEach((subject) => {
-                                        registration.subjectSelection.forEach((subjectSelection) => {
-                                            if (subject.id === subjectSelection.subject) {
-                                                let foundSubject = subject;
-                                                foundSubject.priority = subjectSelection.points;
-                                                foundSubject.selectionId = subjectSelection.id;
-                                                userSubjects.push(foundSubject);
-                                            }
-                                        });
-                                    });
-                                    console.log('found subjects');
-                                    console.log(userSubjects);
-                                    setSubjectSelection(userSubjects);
-                                })
-                                .catch((err) => console.log(`could not fetch subjects! ${err}`));
-                        });
-                }
-            }).catch((err) => console.log(`error! ${err}`));
+  // initially fetch the registration and subject selection of the logged in user
+  useEffect(() => {
+    const loadUsers = async () => {
+      const userInfo = await user.loadUserInfo();
+      setUserInfo(userInfo);
+      if (!subjectSelection) {
+        const registrationResponse = await registrationApi.getRegistration(
+          user.subject,
+          getRequestHeaders(user)
+        );
+        console.log(
+          `registrations of user ${user.subject}: ${registrationResponse.data}`
+        );
+        const registration = registrationResponse.data;
+        if (!registration) {
+          return;
         }
-    }, [user, setUser, subjectSelection, setSubjectSelection]);
 
-    return (
-        <>
-            <Navbar />
-            <BurgerMenu name={URLS.HOME} username={userInfo ? `${userInfo.given_name} ${userInfo.family_name}` : ''}
-                        major={userInfo ? userInfo.degreeCourse : ''}
-                        preferred_username={userInfo ? userInfo.preferred_username : ''}
-                        logout={user ? user.logout : null}
-                        timestamp={userInfo ? userInfo.createTimestamp : '20210911'}
-            />
-            <div className="container main">
-                <div className="row">
-                    <h2>Startseite</h2>
-                    <p>Herzlich Willkommen bei der WPF-Anmeldeseite!<br/>Auf der linken Seite finden Sie ale nötigen
-                        Informationen zur Anmeldung.</p>
-                </div>
-                <div className="row">
-                    <h2>Anmeldefrist</h2>
-                    <p>Die Anmeldefrist für die Wahlpflichtfächer beginnt am 29.4.2021 und endet am 30.05.2021.</p>
-                </div>
-                <div className="row">
-                    <h2>Kontakt</h2>
-                    <p>Bei Fragen/Problemen wenden Sie sich bitte an Frau Bäurle.</p>
-                </div>
-                <div className="row">
-                    <h2>Impressum</h2>
-                    <a href={IMPRINT_URL} style={{marginBottom: '0.5rem'}}>Link</a>
-                </div>
-            </div>
-        </>
-    );
+        const subjectResponse = await subjectApi.getAllSubjects(
+          getRequestHeaders(user)
+        );
+        const userSubjects = [];
+        subjectResponse.data.forEach((subject) => {
+          registration.subjectSelection.forEach((subjectSelection) => {
+            if (subject.id === subjectSelection.subject) {
+              let foundSubject = subject;
+              foundSubject.priority = subjectSelection.points;
+              foundSubject.selectionId = subjectSelection.id;
+              userSubjects.push(foundSubject);
+            }
+          });
+        });
+        console.log("found subjects");
+        console.log(userSubjects);
+        setSubjectSelection(userSubjects);
+      }
+    };
+
+    if (user) {
+      loadUsers().catch(console.error);
+    }
+  }, [user, setUser, subjectSelection, setSubjectSelection]);
+
+  return (
+    <>
+      <Navbar />
+      <BurgerMenu
+        name={URLS.HOME}
+        username={
+          userInfo ? `${userInfo.given_name} ${userInfo.family_name}` : ""
+        }
+        major={userInfo ? userInfo.degreeCourse : ""}
+        preferred_username={userInfo ? userInfo.preferred_username : ""}
+        logout={user ? user.logout : null}
+        timestamp={userInfo ? userInfo.createTimestamp : "20210911"}
+      />
+      <div className="container main">
+        <div className="row">
+          <h2>Startseite</h2>
+          <p>
+            Herzlich Willkommen bei der WPF-Anmeldeseite!
+            <br />
+            Auf der linken Seite finden Sie ale nötigen Informationen zur
+            Anmeldung.
+          </p>
+        </div>
+        <div className="row">
+          <h2>Anmeldefrist</h2>
+          <p>
+            Die Anmeldefrist für die Wahlpflichtfächer beginnt am 29.4.2021 und
+            endet am 30.05.2021.
+          </p>
+        </div>
+        <div className="row">
+          <h2>Kontakt</h2>
+          <p>Bei Fragen/Problemen wenden Sie sich bitte an Frau Bäurle.</p>
+        </div>
+        <div className="row">
+          <h2>Impressum</h2>
+          <a href={IMPRINT_URL} style={{ marginBottom: "0.5rem" }}>
+            Link
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
