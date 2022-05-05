59224ecff2cf097fabb75c8761b4796ca403f241
hanjul <38674358+hanjul@users.noreply.github.com>
2022-05-05 21:58:06 +0200

    Update build dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const path = require('path')
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
   resolve: {
-	  alias: {
-		  '@': path.resolve(__dirname, './src')
-	  },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-})
+});
