6fc93f2fd1887fe13ab7d87396e9980e49fb6828
Dominik Horn <dominik.horn93@gmail.com>
2022-05-12 19:18:45 +0200

    docs: test delegate

---
+++ backend/src/test/java/de/hochschule/augsburg/TestDelegate.java
@@ -0,0 +1,29 @@
+package de.hochschule.augsburg;
+
+import de.hochschule.augsburg.registration.domain.service.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class TestDelegate implements JavaDelegate {
+
+    private final RegistrationService registrationService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        //input -> Welche Variablen brauche ich
+        final String test = delegateExecution.getVariable("fsad").toString();
+
+        //processing -> Aufgabe ausführen
+        registrationService.getAllRegistrations("");
+
+        //output  -> schreibe etwas zurück wenn notwendig
+        delegateExecution.setVariable("tet", "fadsfads");
+
+    }
+}
