1358dc4ad903723c42db4fd2f769f82792d5b1dc
hanjul <38674358+hanjul@users.noreply.github.com>
2022-06-07 15:31:22 +0200

    Switch RegistrationTableItem to ts

--- frontend/src/context/subjectSelectionContext.ts
+++ frontend/src/context/subjectSelectionContext.ts
@@ -1,8 +1,14 @@
+import { SubjectTO } from "@/api";
 import React from "react";
+
+interface SubjectSelectionContextType {
+    subjectSelection: SubjectTO[],
+    setSubjectSelection: (subject: SubjectTO[]) => {}
+}
 
 /*
  * Saves the current subject selection of the user throughout the application.
  */
-const SubjectSelectionContext = React.createContext(null);
+const SubjectSelectionContext = React.createContext<SubjectSelectionContextType | null>(null);
 
 export default SubjectSelectionContext;
